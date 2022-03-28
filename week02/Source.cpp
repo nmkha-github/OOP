773071a94eade0bb1e485f99e611b282fb3d63bd
nmkha-github <nguyenminhkha2001@gmail.com>
2022-03-28 22:00:28 +0700

    Outline

---
+++ week02/Source.cpp
@@ -0,0 +1,6 @@
+#include "bai3.h"
+#include "bai4.h"
+using namespace std;
+int main() {
+	cout << "Hello world";
+}
---
+++ week02/bai3.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template <class T>
+struct node {
+	T data;
+	node* next;
+};
+template <class T>
+class List {
+private:
+	node* head;
+	node* createNode(T val) {
+
+	}
+public:
+
+};
+template <class T>
+class Queue {
+private:
+public:
+};
