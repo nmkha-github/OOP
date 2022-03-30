2691532ea3e6cc249e94b80b7119215da4dee8ec
ArticBlizzard <99569650+ArticBlizzard@users.noreply.github.com>
2022-03-30 20:37:21 +0700

    class graph bai 4

--- week02/bai4.h
+++ week02/bai4.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
 template <class T>
+
 class Vector {
 private:
 	int _size;
@@ -13,35 +15,96 @@ public:
 	int size();
 	T& operator[](int index);
 };
+
 template <class T>
 Vector <T>::Vector() {
 	this->_size = 0;
 	this->arr = new T[200010];
 }
+
 template <class T>
 Vector <T>::~Vector() {
 	delete[] arr;
 }
+
 template <class T>
 void Vector <T>::push_back(T val) {
 	arr[_size++] = val;
 }
+
 template <class T>
 int Vector <T>::size() {
 	return _size;
 }
+
 template <class T>
 T& Vector <T>::operator[](int index) {
 	return arr[index];
 };
 
-class Graph {
-private:
-	
-public:
-	int* BFS();
+
+
+struct DinhKe {
+    int val, cost;
+    DinhKe* next;
 };
+
+struct Canh {
+    int DinhDau, DinhCuoi, TrongSo;
+};
+
+class Graph {
+
+    DinhKe* ThemDinhVaoDanhSachKe(int value, int trongso, DinhKe* head) {
+        DinhKe* newNode = new DinhKe;
+        newNode->val = value;
+        newNode->cost = trongso;
+
+        newNode->next = head;   
+        return newNode;
+    }
+    int N; 
+public:
+    DinhKe** head; // danh sach ke la mang con tro
+
+    // constructor
+    Graph(Canh CacCanh[], int n, int N) {
+        head = new DinhKe * [N]();
+        this->N = N;
+        
+        for (int i = 0; i < N; ++i)
+            head[i] = nullptr;
+      
+        for (unsigned i = 0; i < n; i++) {
+            int DinhDau = CacCanh[i].DinhDau;
+            int DinhCuoi = CacCanh[i].DinhCuoi;
+            int TrongSo = CacCanh[i].TrongSo;
+
+            DinhKe* newNode = ThemDinhVaoDanhSachKe(DinhCuoi, TrongSo, head[DinhDau]);
+
+
+            head[DinhDau] = newNode;
+        }
+    }
+    // destructor
+    ~Graph() {
+        for (int i = 0; i < N; i++)
+            delete[] head[i];
+        delete[] head;
+    }
+    int* BFS();
+};
+
 int* Graph::BFS() {
-	int* res = new int[10];
-	return res;
+    int* res = new int[10];
+    return res;
+}
+void CanhKeCuaMotDinh(DinhKe* ptr, int i)
+{
+    while (ptr != nullptr) {
+        cout << "(" << i << ", " << ptr->val
+            << ", " << ptr->cost << ") ";
+        ptr = ptr->next;
+    }
+    cout << endl;
 }
