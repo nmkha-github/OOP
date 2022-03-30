962eac8ce8e7cec8d6307ca2cc7967cbae9b0bf5
ArticBlizzard <99569650+ArticBlizzard@users.noreply.github.com>
2022-03-30 20:38:18 +0700

    main class graph

--- week02/Source.cpp
+++ week02/Source.cpp
@@ -8,4 +8,21 @@ int main() {
 	for (int i = 0; i < a.size(); i++) cout << a[i] << ' ';
 	
 	cout << "Hello world";
+	/* Canh CacCanh[] = {
+        // (x, y, w) -> canh tu dinh x den dinh y voi trong so la w
+        {0,1,2},{0,2,4},{1,4,3},{2,3,2},{3,1,4},{4,3,3},{1,2,3}
+    };
+
+    int N = 6;  // So dinh cua do thi
+    int n = sizeof(CacCanh) / sizeof(CacCanh[0]);  // tinh so canh
+  
+    Graph graph(CacCanh, n, N);
+
+    cout << "Danh sach ke: " << endl << "(Dinh dau, Dinh cuoi, trong so):" << endl;
+    for (int i = 0; i < N; i++)
+    {
+      
+        CanhKeCuaMotDinh(graph.head[i], i);
+    }
+    return 0; */
 }
