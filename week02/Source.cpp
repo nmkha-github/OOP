9c518cf56343e7efbf80543d33c8fd7cc97e0092
nmkha-github <85301223+nmkha-github@users.noreply.github.com>
2022-03-31 18:29:38 +0700

    Update Source.cpp

--- week02/Source.cpp
+++ week02/Source.cpp
@@ -2,28 +2,42 @@
 #include "bai4.h"
 int main() {
 	//Test vector
-	Vector <int> a;
+	/*Vector <int> a;
 	a.push_back(10);
 	a.push_back(11);
-	for (int i = 0; i < a.size(); i++) cout << a[i] << ' '; cout << endl;
+	for (int i = 0; i < a.size(); i++) cout << a[i] << ' '; cout << endl;*/
 	//Test List
-	List <int> b;
+	/*List <int> b;
 	cout << b.getSize() << endl;
 	b.insertNode(3, b.getSize());
 	cout << b.getSize() << endl;
 	b.insertNode(4, b.getSize());
 	cout << b.getSize() << endl;
-	cout << b.getValueAt(0) << ' ' << b.getValueAt(1) << endl;
+	cout << b.getValueAt(0) << ' ' << b.getValueAt(1) << endl;*/
 	//Test Queue
+	cout << "---------------Bai 3--------------------------\n";
 	Queue <int> q;
+	cout << "Them 1 2 3 vao hang doi.\n";
 	q.push(1);
 	q.push(2);
-	cout << q.top() << ' ' << q.size() << endl;	q.pop();
-	cout << q.top() << ' ' << q.size() << endl; q.pop();
+	q.push(3);
+	cout << "Kich thuoc hang doi sau khi them: " << q.size() << endl;
+	cout << "Gia tri phan tu top: " << q.top() << endl;
+	cout << "Lay 2 phan tu ra khoi hang doi.\n";
 	q.pop();
+	q.pop();
+	cout << "Gia tri phan tu top: " << q.top() << endl;
+	cout << "Kich thuoc hang doi sau khi lay ra: " << q.size() << endl;
+	cout << "Lay 1 phan tu ra khoi hang doi.\n";
+	q.pop();
+	cout << "Kich thuoc hang doi sau khi lay ra: " << q.size() << endl;
 	//Test Graph
+	cout << "---------------Do thi---------------------------\n";
 	Graph G;
 	G.input();
-	G.bfs(0);
-	cout << "\nHello world";
+	int start;
+	cout << "Chon dinh xuat phat: "; cin >> start;
+	Vector <int> res = G.bfs(start);
+	cout << "Thu tu duyet bfs tu " << start << ":\n";
+	for (int i = 0; i < res.size(); i++) cout << res[i] << ' '; cout << endl;
 }
