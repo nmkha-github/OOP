ab5cc5f4b68f6600d35dd85bf56f45af01eb843f
unknown <nguyenminhkha2001@gmail.com>
2022-04-11 17:03:03 +0700

    Update bai1 bai2 java

--- week04/Java/Bai1.java
+++ week04/Java/Bai1.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Bai1 {
+    public static int[] input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so luong phan tu n = ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("arr[" + i + "] = ");
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    public static void output(int arr[]){
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        System.out.print("\n");
+    }
     public static int findMax(int arr[]){
         int temp = arr[0];
         for(int i = 0; i < arr.length; i++){
@@ -9,8 +26,10 @@ public class Bai1 {
         return temp;
     }
     public static void main(String[] args) {
-        int[] a = {5, 3, 4, 8, 1, 10, 9};
-        System.out.println("{5, 3, 4, 8, 1, 10, 9}");
+        int[] a = input();
+        System.out.println("Mang da nhap:");
+        output(a);
+        System.out.println("--------------------Cau1------------------");
         System.out.println("Max = " + findMax(a));
     }
 }
