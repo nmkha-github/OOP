56d6b297b81ca215929a89081e1ce49678c499ad
Github-pha <anhph542@gmail.com>
2022-05-26 21:58:03 +0700

    cài đặt một số lớp

--- FinalProject/Income.cpp
+++ FinalProject/Income.cpp
@@ -3,15 +3,44 @@
 Income::Income(long long wife, long long husband, long long couple, long long other) {
 	this->wifeSalary = wife;
 	this->husbandSalary = husband;
+	this->coupleSalary = couple;
+	this->otherSalary = other;
 }
 
-void Income::setWifeSalary(long long a);
-void Income::setHusbandSalary(long long a);
-void Income::setCoupleSalary(long long a);
-void Income::setOtherSalary(long long a);
-long long Income::getWifeSalary();
-long long Income::getHusbandSalary();
-long long Income::getCoupleSalary();
-long long Income::getOtherSalary();
-long long Income::sumIncome();
+void Income::setWifeSalary(long long a) {
+	this->wifeSalary = a;
+}
+
+void Income::setHusbandSalary(long long a) {
+	this->husbandSalary = a;
+}
+
+void Income::setCoupleSalary(long long a) {
+	this->coupleSalary = a;
+}
+
+void Income::setOtherSalary(long long a) {
+	this->otherSalary = a;
+}
+
+long long Income::getWifeSalary() {
+	return this->wifeSalary;
+}
+
+long long Income::getHusbandSalary() {
+	return this->husbandSalary;
+}
+
+long long Income::getCoupleSalary() {
+	return this->coupleSalary;
+}
+
+long long Income::getOtherSalary() {
+	return this->otherSalary;
+}
+
+long long Income::sumIncome() {
+	return this->husbandSalary + this->wifeSalary + this->coupleSalary + this->otherSalary;
+}
+
 friend std::ostream& operator<<(std::ostream& out, const Income& p);
