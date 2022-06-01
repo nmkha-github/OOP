ead1cba612db8c09f8bbdb909ea5674724385adf
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-06-02 00:15:33 +0700

    fix BankBook class

--- FinalProject/debug/BankBook.h
+++ FinalProject/debug/BankBook.h
@@ -2,24 +2,22 @@
 #ifndef BankBook_h
 #define BankBook_h
 #include <iostream>
+#include "Date.h"
 #include <cmath>
 class BankBook {
 private:
-	struct date {
-		int month, year;
-	};
 	long long moneySaving;
-	date deadline;
-	date startDay;
+	Date deadline;
+	Date startDay;
 	double rate;
 public: 
-	BankBook(long long moneySaving, date startDay, date deadline, double rate);
+	BankBook(long long moneySaving, Date startDay, Date deadline, double rate);
 	BankBook();
 	void addMoney(long long val);
-	void setStartDay(date day);
-	void setDeadline(date day);
+	void setStartDay(Date day);
+	void setDeadline(Date day);
 	void setRate(double val);
-	long long getProfit(date time);
+	long long getProfit(Date time);
 	friend std::ostream& operator<<(std::ostream& out, const BankBook& p);
 };
 #endif // !BankBook_h
